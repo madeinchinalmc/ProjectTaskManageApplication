be3a511e71a3fc315e194f238d920623b5aa1c8d
madeinchinalmc <313801700@qq.com>
2018-02-26 17:59:38 +0800

    plan add Authority

---
+++ ProjectTaskManageApplication/ProjectTaskManageApplication/Filter/AuthorityFilterAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTaskManageApplication.Filter
+{
+    /// <summary>
+    /// 检验权限和登录的filter
+    /// </summary>
+    public class AuthorityFilterAttribute: AuthorizeAttribute
+    {
+        private string _LoginPath = "";
+        public AuthorityFilterAttribute()
+        {
+            this._LoginPath = "/Fourth/Login";
+        }
+    }
+}
